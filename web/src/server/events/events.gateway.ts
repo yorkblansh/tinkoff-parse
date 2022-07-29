38e5d342c43cc776233f9d9a1a072655c29bf71f
Oleg Chepelev <yorkblansh@gmail.com>
2022-07-29 17:54:15 +0300

    update server events

--- web/src/server/events/events.gateway.ts
+++ web/src/server/events/events.gateway.ts
@@ -18,14 +18,14 @@ export class EventsGateway {
 	@WebSocketServer()
 	server: Server
 
-	@SubscribeMessage("events")
-	findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
-		return from([1, 2, 3]).pipe(
-			map((item) => ({ event: "events", data: item }))
-		)
-	}
+	// @SubscribeMessage("events")
+	// findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+	// 	return from([1, 2, 3]).pipe(
+	// 		map((item) => ({ event: "events", data: item }))
+	// 	)
+	// }
 
-	@SubscribeMessage("identity")
+	@SubscribeMessage("parsing")
 	async identity(@MessageBody() data: number): Promise<number> {
 		return data
 	}
