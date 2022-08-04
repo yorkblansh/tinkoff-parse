997a353a99c8ad581c8ecaee890bcd8e8104837c
Oleg Chepelev <yorkblansh@gmail.com>
2022-08-04 14:23:20 +0300

    update service module

--- web/src/server/users/users.service.ts
+++ web/src/server/users/users.service.ts
@@ -1,32 +1,52 @@
 import { Injectable } from "@nestjs/common"
-
-export type User = any
+import { PrismaService } from "prisma/prisma.service"
+import { UserModel } from "../../interfaces/user.interface"
 
 @Injectable()
 export class UsersService {
-	private readonly users: User[]
+	private users: (UserModel | undefined)[]
 
-	constructor() {
-		this.users = [
-			{
-				userId: 1,
-				username: "john",
-				password: "changeme",
-			},
-			{
-				userId: 2,
-				username: "chris",
-				password: "secret",
-			},
-			{
-				userId: 3,
-				username: "maria",
-				password: "guess",
-			},
-		]
+	constructor(private readonly prisma: PrismaService) {
+		// this.users = [
+		// 	{
+		// 		email: "emailll",
+		// 		role: "admin",
+		// 		username: "john",
+		// 		password: "changeme",
+		// 	},
+		// 	{
+		// 		email: "emailll",
+		// 		role: "user",
+		// 		username: "chris",
+		// 		password: "secret",
+		// 	},
+		// 	{
+		// 		email: "emailll",
+		// 		role: "user",
+		// 		username: "maria",
+		// 		password: "guess",
+		// 	},
+		// ]
 	}
 
-	async findOne(username: string): Promise<User | undefined> {
-		return this.users.find((user) => user.username === username)
+	async create(user: UserModel) {
+		const { email, password, username } = user
+		const _user = { email, password, username, role: "user" }
+		try {
+			await this.prisma.user.create({ data: _user })
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	async findOne(username: string) {
+		const [user] = await this.prisma.user.findMany({
+			where: { username },
+		})
+		return user as UserModel
+		// console.log(user)
+		// return this.users.find(
+		// 	(user) => user?.username === username
+		// ) as unknown as Promise<UserModel | undefined>
 	}
 }
