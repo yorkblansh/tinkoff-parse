27c9e300b7092682f3a088a178562764d6277062
Oleg Chepelev <yorkblansh@gmail.com>
2022-08-05 10:07:52 +0300

    move repos to workspace - monorepo

--- web/src/server/auth/decorators/user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common"
-import { UserModel } from "../../../interfaces/user.interface"
-
-export const User = createParamDecorator(
-	(data: unknown, ctx: ExecutionContext) => {
-		const request = ctx.switchToHttp().getRequest()
-		return request.user as UserModel
-	}
-)
---
+++ workspaces/common/index.ts
@@ -0,0 +1,4 @@
+export * from "./interfaces/events.enum";
+export * from "./interfaces/local.http.statuses.enum";
+export * from "./interfaces/routes.interface";
+export * from "./interfaces/user.interface";
