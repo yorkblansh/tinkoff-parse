e71572c46cfb6b5b484f5fc044dbcee6a1854fef
Oleg Chepelev <yorkblansh@gmail.com>
2022-08-06 17:39:17 +0300

    move common to nestjs workspace

--- workspaces/common/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./interfaces/events.enum";
-export * from "./interfaces/local.http.statuses.enum";
-export * from "./interfaces/routes.interface";
-export * from "./interfaces/user.interface";
