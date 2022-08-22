c67534cc533c0c9df27db8e0f21f68bd532b1b72
Oleg Chepelev <yorkblansh@gmail.com>
2022-08-22 22:10:42 +0300

    update roles decorators

--- web/workspaces/nestjs/src/auth/decorators/roles.decorator.ts
+++ web/workspaces/nestjs/src/auth/decorators/roles.decorator.ts
@@ -1,5 +1,8 @@
 import { SetMetadata } from "@nestjs/common"
+import { ApiUnauthorizedResponse } from "@nestjs/swagger"
 import { UserRole } from "common/interfaces/user.interface"
 
+// const role = "admin"
+
 export const ROLES_KEY = "roles"
-export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles)
+export const AdminCheck = (...role: UserRole[]) => SetMetadata(ROLES_KEY, role)
