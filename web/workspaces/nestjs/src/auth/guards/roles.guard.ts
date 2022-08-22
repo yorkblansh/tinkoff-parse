e7f38f93f0f516ed93fe2c3bd98ca7bee01aa2ed
Oleg Chepelev <yorkblansh@gmail.com>
2022-08-22 22:12:25 +0300

    update roles.guard

--- web/workspaces/nestjs/src/auth/guards/roles.guard.ts
+++ web/workspaces/nestjs/src/auth/guards/roles.guard.ts
@@ -1,9 +1,18 @@
 import { HttpService } from "@nestjs/axios"
-import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common"
+import {
+	Injectable,
+	CanActivate,
+	ExecutionContext,
+	HttpException,
+	ForbiddenException,
+	UnauthorizedException,
+} from "@nestjs/common"
 import { Reflector } from "@nestjs/core"
 import { AuthService } from "auth/auth.service"
 import { ROLES_KEY } from "auth/decorators/roles.decorator"
+import { is } from "cheerio/lib/api/traversing"
 import { UserRole } from "common/interfaces/user.interface"
+import { Response } from "express"
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -12,7 +21,7 @@ export class RolesGuard implements CanActivate {
 		private readonly httpService: HttpService
 	) {}
 
-	async canActivate(context: ExecutionContext): Promise<boolean> {
+	async canActivate(context: ExecutionContext) {
 		const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
 			ROLES_KEY,
 			[context.getHandler(), context.getClass()]
@@ -22,27 +31,43 @@ export class RolesGuard implements CanActivate {
 			return true
 		}
 
+		// const res = context.switchToHttp().getResponse<Response>()
 		const httpRequest = context.switchToHttp().getRequest()
-		const wsData = context.switchToWs().getData()
-		console.log(wsData)
-		// const roles = await this.authService.validateUserRole(user.username)
-		// const db_data = this.prisma.user.findMany({
-		// 	where: { username: user.username },
-		// })
-		// this.authService.validateUserRole()
-		const { data: profile } = await this.httpService.axiosRef.get(
-			"http://localhost:3001/api/profile",
-			{
+		// const wsData = context.switchToWs().getData()
+
+		return await this.httpService.axiosRef
+			.get("http://localhost:3001/api/profile", {
 				headers: {
 					...httpRequest.headers,
 					// Authorization: `Bearer ${wsData.access_token}`,
 				},
-			}
-		)
-		// console.log(profile)
-		// console.log(`test /api/p:    ${profile}`)
-		// console.log(request.headers.authorization)
-		// return true
-		return requiredRoles.some((role) => profile.role === role)
+			})
+			.then((response) => {
+				const { data: profile } = response
+				return requiredRoles.some((role) => profile.role === role)
+				// const isOK = requiredRoles.some((role) => profile.role === role)
+				// THIS IS FOR SPEC EXCEPTION
+				// return this.validateRequest(isOK)
+			})
+			.catch((e) => {
+				// console.log("api/profile exception")
+				// console.log(e.response.data)
+				const errorStatusCode = e.response.data?.statusCode
+				if (errorStatusCode && errorStatusCode === 401) {
+					throw new UnauthorizedException()
+				} else {
+					throw new HttpException("User is not admin", 455)
+				}
+			})
 	}
+
+	// private validateRequest(isOK: boolean) {
+	// 	if (isOK) {
+	// 		return true
+	// 	} else {
+	// 		// console.log("user__is__not__admin")
+	// 		// console.log(`isOK: ${isOK}`)
+	// 		throw new HttpException("User is not admin", 455)
+	// 	}
+	// }
 }
